48cb7c660865bbbb18ef6399c283c795d9e1b05d
Fagbayibo <fagbayibooluwasegun@gmail.com>
2024-12-26 14:00:19 +0100

    Feat: added the update function to update movies based on id

--- main.go
+++ main.go
@@ -60,8 +60,30 @@ func createMovie(w http.ResponseWriter, r *http.Request) {
 	var movie Movie
 	_ = json.NewDecoder(r.Body).Decode(&movie)
 	movie.ID = strconv.Itoa(rand.Intn(10000000))
-
 	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	//set json content type
+	w.Header().Set("Content-Type", "application/json")
+	// params to get id
+	params := mux.Vars(r)
+	// loop over the movie, range,
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+		}
+
+	}
+
+	//delete thee movie with the id
+	//add a new movie - the movie that we send in the body of postt man
 }
 
 func main() {
